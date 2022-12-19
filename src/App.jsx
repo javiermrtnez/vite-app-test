5880fa00e1c096daa6f8c02f33dbd1c0eda92ebd
javiermrtnez <javiermartinezbueno@gmail.com>
2022-12-20 00:28:56 +0100

    Adding dist

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,12 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img
-            src={caraLuis}
-            // height="50px"
-            // width="50px"
-            className="logo react"
-            alt="React logo"
-          />
+        <a href="#" target="_blank">
+          <img src={caraLuis} className="logo" alt="React logo" />
         </a>
       </div>
+
       <h1>Luis culo gordo</h1>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </div>
   );
 }
